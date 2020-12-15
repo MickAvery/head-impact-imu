612f34360fa0ff86237cbe8988323d3c69ee86cc
MickAvery <mav.cuyugan@gmail.com>
2020-12-15 09:48:10 -0800

    removed advertising timeout for indefinite advertising, also reduced advertising frequency to consume less power

--- src/network.c
+++ src/network.c
@@ -27,8 +27,8 @@
 
 #define DEVICE_NAME                     "SimpLab-IMU" /*!< Name of device to be included in advertising data. */
 
-#define APP_ADV_INTERVAL                300   /*!< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
-#define APP_ADV_DURATION                18000 /*!< The advertising duration (180 seconds) in units of 10 milliseconds. */
+#define APP_ADV_INTERVAL                600   /*!< The advertising interval (in units of 0.625 ms. This value corresponds to 375 ms). */
+#define APP_ADV_DURATION                0     /*!< The advertising duration (180 seconds) in units of 10 milliseconds. */
 
 #define APP_BLE_OBSERVER_PRIO           3 /*!< Application's BLE observer priority. You shouldn't need to modify this value. (NOT SURE WHAT THIS IS FOR) */
 #define APP_BLE_CONN_CFG_TAG            1 /*!< A tag identifying the SoftDevice BLE configuration (NOT SURE WHAT THIS IS FOR) */
